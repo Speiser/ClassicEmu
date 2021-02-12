c5db97ec1f1972a2e382856f9ab4924ee3e642cf
Bernhard Speiser <bernhard.speiser@yahoo.com>
2021-02-12 18:23:18 +0100

    Handle CMSG_UPDATE_ACCOUNT_DATA

---
+++ src/World/Handler/AccountHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Classic.World.Messages.Client;
+
+namespace Classic.World.Handler
+{
+    public class AccountHandler
+    {
+        [OpcodeHandler(Opcode.CMSG_UPDATE_ACCOUNT_DATA)]
+        public static Task UpdateAccountData(WorldClient client, byte[] data)
+        {
+            var request = new CMSG_UPDATE_ACCOUNT_DATA(data);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/World/Messages/Client/CMSG_UPDATE_ACCOUNT_DATA.cs
@@ -0,0 +1,19 @@
+﻿using Classic.Common;
+
+namespace Classic.World.Messages.Client
+{
+    public class CMSG_UPDATE_ACCOUNT_DATA
+    {
+        public CMSG_UPDATE_ACCOUNT_DATA(byte[] data)
+        {
+            using var reader = new PacketReader(data);
+            Type = reader.ReadUInt32();
+            Size = reader.ReadUInt32();
+            // Data = ???
+        }
+
+        public uint Type { get; }
+        public uint Size { get; }
+        public string Data { get; }
+    }
+}
