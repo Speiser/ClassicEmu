3b68cc3c36682d6801523f83096ae831a3c71eae
Bernhard Speiser <bernhard.speiser@yahoo.com>
2021-02-12 17:49:17 +0100

    Handle CMSG_ZONEUPDATE

---
+++ src/World/Handler/ZoneHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Classic.Data.Enums.Map;
+using Classic.World.Messages.Client;
+
+namespace Classic.World.Handler
+{
+    class ZoneHandler
+    {
+        [OpcodeHandler(Opcode.CMSG_ZONEUPDATE)]
+        public static Task OnZoneUpdate(WorldClient client, byte[] data)
+        {
+            var request = new CMSG_ZONEUPDATE(data);
+
+            client.Log($"{client.Player.Name} entered {(ZoneID)request.NewZone}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/World/Messages/Client/CMSG_ZONEUPDATE.cs
@@ -0,0 +1,15 @@
+﻿using Classic.Common;
+
+namespace Classic.World.Messages.Client
+{
+    public class CMSG_ZONEUPDATE
+    {
+        public CMSG_ZONEUPDATE(byte[] data)
+        {
+            using var reader = new PacketReader(data);
+            NewZone = reader.ReadUInt32();
+        }
+
+        public uint NewZone { get; }
+    }
+}
