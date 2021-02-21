6667aeb98f4f3d3a2be109f79b1f0e8c9a6b4431
Bernhard Speiser <bernhard.speiser@yahoo.com>
2021-02-21 20:26:23 +0100

    Rework OnPlayerLogin for tbc

--- src/World/Handler/PlayerHandler.cs
+++ src/World/Handler/PlayerHandler.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Classic.Data;
+using Classic.World.Extensions;
+using Classic.World.Messages;
 using Classic.World.Messages.Client;
 using Classic.World.Messages.Server;
 
@@ -15,36 +19,9 @@ namespace Classic.World.Handler
 
             args.Client.Log($"Player logged in with char {character.Name}");
 
-            await args.Client.SendPacket(new SMSG_LOGIN_VERIFY_WORLD(character));
-            await args.Client.SendPacket(new SMSG_ACCOUNT_DATA_TIMES());
-
-            await args.Client.SendPacket(new SMSG_SET_REST_START());
-            await args.Client.SendPacket(new SMSG_BINDPOINTUPDATE(character));
-            await args.Client.SendPacket(new SMSG_TUTORIAL_FLAGS());
-            await args.Client.SendPacket(new SMSG_LOGIN_SETTIMESPEED());
-
-            await args.Client.SendPacket(new SMSG_INITIAL_SPELLS(character.Spells));
-            await args.Client.SendPacket(new SMSG_ACTION_BUTTONS(character.ActionBar));
-            await args.Client.SendPacket(new SMSG_INITIALIZE_FACTIONS());
-
-            // TODO: not working
-            // await args.Client.SendPacket(new SMSG_TRIGGER_CINEMATIC(CinematicID.NightElf));
-
-            await args.Client.SendPacket(new SMSG_CORPSE_RECLAIM_DELAY());
-            await args.Client.SendPacket(new SMSG_INIT_WORLD_STATES());
-            await args.Client.SendPacket(SMSG_UPDATE_OBJECT.CreateOwnPlayerObject(character, out var player));
-
-            // Initially spawn all creatures
-            foreach (var unit in args.WorldState.Creatures)
-            {
-                // TODO: Add range check
-                await args.Client.SendPacket(SMSG_UPDATE_OBJECT.CreateUnit(unit));
-            }
-
-            args.Client.Player = player;
-            await args.WorldState.SpawnPlayer(character);
-            
-            await args.Client.SendPacket(new SMSG_MESSAGECHAT(character.ID, "Hello World"));
+            if (args.IsVanilla()) await OnPlayerLoginVanilla(args, character);
+            else if (args.IsTBC()) await OnPlayerLoginTBC(args, character);
+            else throw new NotImplementedException($"SMSG_UPDATE_OBJECT(build: {args.Client.Build})");
         }
 
         [OpcodeHandler(Opcode.CMSG_LOGOUT_REQUEST)]
@@ -54,6 +31,85 @@ namespace Classic.World.Handler
             args.Client.Player = null;
 
             // TODO: Remove from other clients
+        }
+
+        private static async Task OnPlayerLoginVanilla(HandlerArguments args, Character character)
+        {
+            await args.Client.SendPacket(new SMSG_LOGIN_VERIFY_WORLD(character));
+            await args.SendPacket<SMSG_ACCOUNT_DATA_TIMES>();
+            //SMSG_EXPECTED_SPAM_RECORDS
+            await args.Client.SendPacket(new SMSG_MESSAGECHAT(character.ID, "https://github.com/Speiser/ClassicEmu"));
+            //await args.Client.SendPacket(new SMSG_NAME_QUERY_RESPONSE(character, args.Client.Build));
+            //if (GUILD) -> SMSG_GUILD_EVENT
+            if (character.Stats.Life == 0)
+            {
+                await args.SendPacket<SMSG_CORPSE_RECLAIM_DELAY>();
+            }
+            await args.SendPacket<SMSG_SET_REST_START>();
+            await args.Client.SendPacket(new SMSG_BINDPOINTUPDATE(character));
+            await args.SendPacket<SMSG_TUTORIAL_FLAGS>();
+            await args.Client.SendPacket(new SMSG_INITIAL_SPELLS(character.Spells));
+            //SMSG_SEND_UNLEARN_SPELLS
+            await args.Client.SendPacket(new SMSG_ACTION_BUTTONS(character.ActionBar));
+            await args.SendPacket<SMSG_INITIALIZE_FACTIONS>();
+            await args.SendPacket<SMSG_LOGIN_SETTIMESPEED>();
+            // await args.Client.SendPacket(new SMSG_TRIGGER_CINEMATIC(CinematicID.NightElf));
+
+            await args.Client.SendPacket(SMSG_UPDATE_OBJECT_VANILLA.CreateOwnPlayerObject(character, out var player));
+
+            // Initially spawn all creatures
+            foreach (var unit in args.WorldState.Creatures)
+            {
+                // TODO: Add range check
+                await args.Client.SendPacket(SMSG_UPDATE_OBJECT_VANILLA.CreateUnit(unit));
+            }
+
+            args.Client.Player = player;
+            await args.WorldState.SpawnPlayer(character);
+
+            //if (GROUP) ->SMSG_GROUP_LIST
+            //SMSG_FRIEND_LIST
+            //SMSG_IGNORE_LIST
+            //SMSG_ITEM_ENCHANT_TIME_UPDATE
+            //SMSG_ITEM_TIME_UPDATE
+            //SMSG_FRIEND_STATUS
+        }
+
+        private static async Task OnPlayerLoginTBC(HandlerArguments args, Character character)
+        {
+            await args.SendPacket<MSG_SET_DUNGEON_DIFFICULTY>();
+            await args.Client.SendPacket(new SMSG_LOGIN_VERIFY_WORLD(character));
+            await args.SendPacket<SMSG_ACCOUNT_DATA_TIMES>();
+            await args.SendPacket<SMSG_FEATURE_SYSTEM_STATUS>();
+            //SMSG_EXPECTED_SPAM_RECORDS(WorldSession::SendExpectedSpamRecords)
+            await args.Client.SendPacket(new SMSG_MESSAGECHAT(character.ID, "https://github.com/Speiser/ClassicEmu")); // TODO Use SMSG_MOTD
+            //await args.Client.SendPacket(new SMSG_NAME_QUERY_RESPONSE(character, args.Client.Build));
+            //if (GUILD) -> SMSG_GUILD_EVENT(WorldSession::HandlePlayerLogin)
+            if (character.Stats.Life == 0)
+            {
+                await args.SendPacket<SMSG_CORPSE_RECLAIM_DELAY>();
+            }
+            await args.SendPacket<SMSG_SET_REST_START>();
+            await args.Client.SendPacket(new SMSG_BINDPOINTUPDATE(character));
+            await args.SendPacket<SMSG_TUTORIAL_FLAGS>();
+            await args.SendPacket<SMSG_INSTANCE_DIFFICULTY>();
+            await args.Client.SendPacket(new SMSG_INITIAL_SPELLS(character.Spells));
+            //SMSG_SEND_UNLEARN_SPELLS(Player::SendUnlearnSpells)
+            await args.Client.SendPacket(new SMSG_ACTION_BUTTONS(character.ActionBar));
+            await args.SendPacket<SMSG_INITIALIZE_FACTIONS>();
+            await args.SendPacket<SMSG_LOGIN_SETTIMESPEED>();
+            // await args.Client.SendPacket(new SMSG_TRIGGER_CINEMATIC(CinematicID.NightElf));
+            
+            await args.Client.SendPacket(SMSG_UPDATE_OBJECT_TBC.CreateOwnPlayerObject(character, out var player));
+
+            //if (GROUP) -> SMSG_GROUP_LIST(Group::SendUpdateTo)
+            //- "NEW" - SMSG_CONTACT_LIST(PlayerSocial::SendSocialList)
+            await args.SendPacket<SMSG_TIME_SYNC_REQ>();
+            //SMSG_ITEM_ENCHANT_TIME_UPDATE(WorldSession::SendItemEnchantTimeUpdate)
+            //SMSG_ITEM_TIME_UPDATE(Item::SendTimeUpdate)
+            //SMSG_FRIEND_STATUS(SocialMgr::MakeFriendStatusPacket)
+
+            args.Client.Player = player;
         }
     }
 }
