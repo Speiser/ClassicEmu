8034af74554832461cc14975a6240ba7f7dcb9f9
Bernhard Speiser <bernhard.speiser@yahoo.com>
2021-02-26 16:16:48 +0100

    Merge CreateOwnPlayerObject and OnPlayerLogin

--- src/World/Handler/PlayerHandler.cs
+++ src/World/Handler/PlayerHandler.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Classic.Common;
-using Classic.Data;
 using Classic.World.Extensions;
 using Classic.World.Messages;
 using Classic.World.Messages.Client;
@@ -12,6 +10,8 @@ namespace Classic.World.Handler
 {
     public static class PlayerHandler
     {
+        private const string MessageOfTheDay = "https://github.com/Speiser/ClassicEmu";
+
         [OpcodeHandler(Opcode.CMSG_PLAYER_LOGIN)]
         public static async Task OnPlayerLogin(HandlerArguments args)
         {
@@ -20,9 +20,85 @@ namespace Classic.World.Handler
 
             args.Client.Log($"Player logged in with char {character.Name}");
 
-            if (args.IsVanilla()) await OnPlayerLoginVanilla(args, character);
-            else if (args.IsTBC()) await OnPlayerLoginTBC(args, character);
-            else throw new NotImplementedException($"SMSG_UPDATE_OBJECT(build: {args.Client.Build})");
+            if (args.IsTBC())
+            {
+                await args.SendPacket<MSG_SET_DUNGEON_DIFFICULTY>();
+            }
+
+            await args.Client.SendPacket(new SMSG_LOGIN_VERIFY_WORLD(character));
+            await args.SendPacket<SMSG_ACCOUNT_DATA_TIMES>();
+
+            if (args.IsTBC())
+            {
+                await args.SendPacket<SMSG_FEATURE_SYSTEM_STATUS>();
+            }
+
+            await args.Client.SendPacket(new SMSG_EXPECTED_SPAM_RECORDS(Enumerable.Empty<string>()));
+
+            if (args.IsVanilla())
+            {
+                await args.Client.SendPacket(new SMSG_MESSAGECHAT(character.ID, MessageOfTheDay));
+            }
+            else if (args.IsTBC())
+            {
+                await args.Client.SendPacket(new SMSG_MOTD(MessageOfTheDay));
+            }
+
+            // await args.Client.SendPacket(new SMSG_NAME_QUERY_RESPONSE(character, args.Client.Build));
+            // if (GUILD) -> SMSG_GUILD_EVENT
+
+            if (character.Stats.Life == 0)
+            {
+                await args.SendPacket<SMSG_CORPSE_RECLAIM_DELAY>();
+            }
+
+            await args.SendPacket<SMSG_SET_REST_START>();
+            await args.Client.SendPacket(new SMSG_BINDPOINTUPDATE(character));
+            await args.SendPacket<SMSG_TUTORIAL_FLAGS>();
+
+            if (args.IsTBC())
+            {
+                await args.SendPacket<SMSG_INSTANCE_DIFFICULTY>();
+            }
+
+            await args.Client.SendPacket(new SMSG_INITIAL_SPELLS(character.Spells));
+            // SMSG_SEND_UNLEARN_SPELLS
+            await args.Client.SendPacket(new SMSG_ACTION_BUTTONS(character.ActionBar));
+            await args.Client.SendPacket(new SMSG_INITIALIZE_FACTIONS(ClientBuild.Vanilla));
+            await args.SendPacket<SMSG_LOGIN_SETTIMESPEED>();
+            // await args.Client.SendPacket(new SMSG_TRIGGER_CINEMATIC(CinematicID.NightElf));
+
+            await args.Client.SendPacket(SMSG_UPDATE_OBJECT.CreateOwnPlayerObject(character, args.Client.Build, out var player));
+            args.Client.Player = player;
+
+            // TODO: Implement for TBC
+            if (args.IsVanilla())
+            {
+                // Initially spawn all creatures
+                foreach (var unit in args.WorldState.Creatures)
+                {
+                    // TODO: Add range check
+                    await args.Client.SendPacket(SMSG_UPDATE_OBJECT_VANILLA.CreateUnit(unit));
+                }
+
+                await args.WorldState.SpawnPlayer(character);
+            }
+
+            // if (GROUP) -> SMSG_GROUP_LIST
+            // if Vanilla
+            //     SMSG_FRIEND_LIST
+            //     SMSG_IGNORE_LIST
+            // if TBC
+            //     SMSG_CONTACT_LIST
+
+            if (args.IsTBC())
+            {
+                await args.SendPacket<SMSG_TIME_SYNC_REQ>();
+            }
+            
+            // SMSG_ITEM_ENCHANT_TIME_UPDATE
+            // SMSG_ITEM_TIME_UPDATE
+            // SMSG_FRIEND_STATUS
         }
 
         [OpcodeHandler(Opcode.CMSG_LOGOUT_REQUEST)]
@@ -32,85 +108,6 @@ namespace Classic.World.Handler
             args.Client.Player = null;
 
             // TODO: Remove from other clients
-        }
-
-        private static async Task OnPlayerLoginVanilla(HandlerArguments args, Character character)
-        {
-            await args.Client.SendPacket(new SMSG_LOGIN_VERIFY_WORLD(character));
-            await args.SendPacket<SMSG_ACCOUNT_DATA_TIMES>();
-            //SMSG_EXPECTED_SPAM_RECORDS
-            await args.Client.SendPacket(new SMSG_MESSAGECHAT(character.ID, "https://github.com/Speiser/ClassicEmu"));
-            //await args.Client.SendPacket(new SMSG_NAME_QUERY_RESPONSE(character, args.Client.Build));
-            //if (GUILD) -> SMSG_GUILD_EVENT
-            if (character.Stats.Life == 0)
-            {
-                await args.SendPacket<SMSG_CORPSE_RECLAIM_DELAY>();
-            }
-            await args.SendPacket<SMSG_SET_REST_START>();
-            await args.Client.SendPacket(new SMSG_BINDPOINTUPDATE(character));
-            await args.SendPacket<SMSG_TUTORIAL_FLAGS>();
-            await args.Client.SendPacket(new SMSG_INITIAL_SPELLS(character.Spells));
-            //SMSG_SEND_UNLEARN_SPELLS
-            await args.Client.SendPacket(new SMSG_ACTION_BUTTONS(character.ActionBar));
-            await args.Client.SendPacket(new SMSG_INITIALIZE_FACTIONS(ClientBuild.Vanilla));
-            await args.SendPacket<SMSG_LOGIN_SETTIMESPEED>();
-            // await args.Client.SendPacket(new SMSG_TRIGGER_CINEMATIC(CinematicID.NightElf));
-
-            await args.Client.SendPacket(SMSG_UPDATE_OBJECT_VANILLA.CreateOwnPlayerObject(character, out var player));
-
-            // Initially spawn all creatures
-            foreach (var unit in args.WorldState.Creatures)
-            {
-                // TODO: Add range check
-                await args.Client.SendPacket(SMSG_UPDATE_OBJECT_VANILLA.CreateUnit(unit));
-            }
-
-            args.Client.Player = player;
-            await args.WorldState.SpawnPlayer(character);
-
-            //if (GROUP) ->SMSG_GROUP_LIST
-            //SMSG_FRIEND_LIST
-            //SMSG_IGNORE_LIST
-            //SMSG_ITEM_ENCHANT_TIME_UPDATE
-            //SMSG_ITEM_TIME_UPDATE
-            //SMSG_FRIEND_STATUS
-        }
-
-        private static async Task OnPlayerLoginTBC(HandlerArguments args, Character character)
-        {
-            await args.SendPacket<MSG_SET_DUNGEON_DIFFICULTY>();
-            await args.Client.SendPacket(new SMSG_LOGIN_VERIFY_WORLD(character));
-            await args.SendPacket<SMSG_ACCOUNT_DATA_TIMES>();
-            await args.SendPacket<SMSG_FEATURE_SYSTEM_STATUS>();
-            await args.Client.SendPacket(new SMSG_EXPECTED_SPAM_RECORDS(Enumerable.Empty<string>()));
-            await args.Client.SendPacket(new SMSG_MOTD("https://github.com/Speiser/ClassicEmu"));
-            //await args.Client.SendPacket(new SMSG_NAME_QUERY_RESPONSE(character, args.Client.Build));
-            //if (GUILD) -> SMSG_GUILD_EVENT(WorldSession::HandlePlayerLogin)
-            if (character.Stats.Life == 0)
-            {
-                await args.SendPacket<SMSG_CORPSE_RECLAIM_DELAY>();
-            }
-            await args.SendPacket<SMSG_SET_REST_START>();
-            await args.Client.SendPacket(new SMSG_BINDPOINTUPDATE(character));
-            await args.SendPacket<SMSG_TUTORIAL_FLAGS>();
-            await args.SendPacket<SMSG_INSTANCE_DIFFICULTY>();
-            await args.Client.SendPacket(new SMSG_INITIAL_SPELLS(character.Spells));
-            //SMSG_SEND_UNLEARN_SPELLS(Player::SendUnlearnSpells)
-            await args.Client.SendPacket(new SMSG_ACTION_BUTTONS(character.ActionBar));
-            await args.Client.SendPacket(new SMSG_INITIALIZE_FACTIONS(ClientBuild.TBC));
-            await args.SendPacket<SMSG_LOGIN_SETTIMESPEED>();
-            // await args.Client.SendPacket(new SMSG_TRIGGER_CINEMATIC(CinematicID.NightElf));
-
-            await args.Client.SendPacket(SMSG_UPDATE_OBJECT_TBC.CreateOwnPlayerObject(character, out var player));
-
-            //if (GROUP) -> SMSG_GROUP_LIST(Group::SendUpdateTo)
-            //- "NEW" - SMSG_CONTACT_LIST(PlayerSocial::SendSocialList)
-            await args.SendPacket<SMSG_TIME_SYNC_REQ>();
-            //SMSG_ITEM_ENCHANT_TIME_UPDATE(WorldSession::SendItemEnchantTimeUpdate)
-            //SMSG_ITEM_TIME_UPDATE(Item::SendTimeUpdate)
-            //SMSG_FRIEND_STATUS(SocialMgr::MakeFriendStatusPacket)
-
-            args.Client.Player = player;
         }
     }
 }
--- src/World/Messages/Server/SMSG_UPDATE_OBJECT.cs
+++ src/World/Messages/Server/SMSG_UPDATE_OBJECT.cs
@@ -8,13 +8,13 @@ using Classic.World.Extensions;
 
 namespace Classic.World.Messages.Server
 {
-    public class SMSG_UPDATE_OBJECT_TBC : ServerMessageBase<Opcode>
+    public class SMSG_UPDATE_OBJECT : ServerMessageBase<Opcode>
     {
-        public SMSG_UPDATE_OBJECT_TBC() : base(Opcode.SMSG_UPDATE_OBJECT) { }
+        public SMSG_UPDATE_OBJECT() : base(Opcode.SMSG_UPDATE_OBJECT) { }
 
-        public static SMSG_UPDATE_OBJECT_TBC CreateOwnPlayerObject(Character character, out PlayerEntity player)
+        public static SMSG_UPDATE_OBJECT CreateOwnPlayerObject(Character character, int build, out PlayerEntity player)
         {
-            var update = new SMSG_UPDATE_OBJECT_TBC();
+            var update = new SMSG_UPDATE_OBJECT();
 
             update.Writer
                 .WriteUInt32(1) // blocks.Count
@@ -23,32 +23,51 @@ namespace Classic.World.Messages.Server
                 .WriteUInt8((byte)ObjectUpdateType.UPDATETYPE_CREATE_OBJECT_SELF)
                 .WriteBytes(character.ID.ToPackedUInt64())
 
-                .WriteUInt8((byte)TypeId.TypeidPlayer)
-                .WriteUInt8((byte)(ObjectUpdateFlag_TBC.Highguid |
-                                   ObjectUpdateFlag_TBC.HasPosition |
-                                   ObjectUpdateFlag_TBC.Living |
-                                   ObjectUpdateFlag_TBC.Self))
+                .WriteUInt8((byte)TypeId.TypeidPlayer);
 
-                .WriteUInt32((uint)MovementFlags.None)
-                .WriteUInt8(0) // moveFlags2
+            var updateFlags = build switch
+            {
+                ClientBuild.Vanilla => (byte)(ObjectUpdateFlag_VANILLA.All
+                                       | ObjectUpdateFlag_VANILLA.HasPosition
+                                       | ObjectUpdateFlag_VANILLA.Living
+                                       | ObjectUpdateFlag_VANILLA.Self),
+                ClientBuild.TBC => (byte)(ObjectUpdateFlag_TBC.Highguid
+                                   | ObjectUpdateFlag_TBC.HasPosition
+                                   | ObjectUpdateFlag_TBC.Living
+                                   | ObjectUpdateFlag_TBC.Self),
+                _ => throw new NotImplementedException($"GetUpdateFlags(build: {build})"),
+            };
+
+            update.Writer
+                .WriteUInt8(updateFlags)
+                .WriteUInt32((uint)MovementFlags.None);
+
+            if (build == ClientBuild.TBC) update.Writer.WriteUInt8(0); // moveFlags2
+
+            update.Writer
                 .WriteUInt32((uint)Environment.TickCount)
-
                 .WriteMap(character.Position)
 
-                .WriteFloat(0x00000015) // ??
+                .WriteFloat(0) // ??
 
                 .WriteFloat(2.5f) // WalkSpeed
                 .WriteFloat(7f) // RunSpeed
                 .WriteFloat(2.5f) // Backwards WalkSpeed
                 .WriteFloat(4.72f) // SwimSpeed
-                .WriteFloat(2.5f) // Backwards SwimSpeed
-                .WriteFloat(14f) // MOVE_FLIGHT
-                .WriteFloat(14f) // MOVE_FLIGHT_BACK
-                .WriteFloat(3.14f) // TurnSpeed
+                .WriteFloat(2.5f); // Backwards SwimSpeed
 
+            if (build == ClientBuild.TBC)
+            {
+                update.Writer
+                    .WriteFloat(14f) // MOVE_FLIGHT
+                    .WriteFloat(14f); // MOVE_FLIGHT_BACK
+            }
+
+            update.Writer
+                .WriteFloat(3.14f) // TurnSpeed
                 .WriteUInt32(0); // ??
 
-            player = new PlayerEntity(character, ClientBuild.TBC)
+            player = new PlayerEntity(character, build)
             {
                 ObjectGuid = new ObjectGuid(character.ID),
                 Guid = character.ID
@@ -64,49 +83,6 @@ namespace Classic.World.Messages.Server
     public class SMSG_UPDATE_OBJECT_VANILLA : ServerMessageBase<Opcode>
     {
         public SMSG_UPDATE_OBJECT_VANILLA() : base(Opcode.SMSG_UPDATE_OBJECT) { }
-
-        public static SMSG_UPDATE_OBJECT_VANILLA CreateOwnPlayerObject(Character character, out PlayerEntity player)
-        {
-            var update = new SMSG_UPDATE_OBJECT_VANILLA();
-
-            update.Writer
-                .WriteUInt32(1) // blocks.Count
-                .WriteUInt8(0) // hasTransport
-
-                .WriteUInt8((byte) ObjectUpdateType.UPDATETYPE_CREATE_OBJECT_SELF)
-                .WriteBytes(character.ID.ToPackedUInt64())
-
-                .WriteUInt8((byte) TypeId.TypeidPlayer)
-                .WriteUInt8((byte) (ObjectUpdateFlag_VANILLA.All |
-                                    ObjectUpdateFlag_VANILLA.HasPosition |
-                                    ObjectUpdateFlag_VANILLA.Living |
-                                    ObjectUpdateFlag_VANILLA.Self))
-
-                .WriteUInt32((uint) MovementFlags.None)
-                .WriteUInt32((uint) Environment.TickCount)
-
-                .WriteMap(character.Position)
-
-                .WriteFloat(0) // ??
-
-                .WriteFloat(2.5f) // WalkSpeed
-                .WriteFloat(7f) // RunSpeed
-                .WriteFloat(2.5f) // Backwards WalkSpeed
-                .WriteFloat(4.72f) // SwimSpeed
-                .WriteFloat(2.5f) // Backwards SwimSpeed
-                .WriteFloat(3.14f) // TurnSpeed
-
-                .WriteInt32(1); // ??
-
-            player = new PlayerEntity(character, ClientBuild.Vanilla)
-            {
-                ObjectGuid = new ObjectGuid(character.ID),
-                Guid = character.ID
-            };
-
-            player.WriteUpdateFields(update.Writer);
-            return update;
-        }
 
         public static SMSG_UPDATE_OBJECT_VANILLA CreatePlayer(Character character)
         {
