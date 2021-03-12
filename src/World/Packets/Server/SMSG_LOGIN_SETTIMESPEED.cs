cb6ec940a1334cd452ed596c31bee9f59dd06643
Bernhard Speiser <bernhard.speiser@yahoo.com>
2021-03-12 20:40:56 +0100

    Calculate time

--- src/World/Packets/Server/SMSG_LOGIN_SETTIMESPEED.cs
+++ src/World/Packets/Server/SMSG_LOGIN_SETTIMESPEED.cs
@@ -9,8 +9,18 @@ namespace Classic.World.Packets.Server
         }
 
         public override byte[] Get() => this.Writer
-            .WriteUInt32(Convert.ToUInt32((DateTime.Now - new DateTime(1970, 1, 1)).TotalSeconds)) // TIME
+            .WriteUInt32(CalculateCurrentTime()) // TIME
             .WriteFloat(0.01666667f) // Speed
             .Build();
+
+        private static uint CalculateCurrentTime()
+        {
+            var time = DateTime.Now;
+            var year = time.Year - 2000;
+            var month = time.Month - 1;
+            var day = time.Day - 1;
+
+            return (uint)(time.Minute | (time.Hour << 6) | ((int)time.DayOfWeek << 11) | (day << 14) | (month << 20) | (year << 24));
+        }
     }
 }
