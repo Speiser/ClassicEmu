6fd39dd9538441f69b526e6da67c196ae2e7407f
Bernhard Speiser <bernhard.speiser@yahoo.com>
2022-02-19 18:01:27 +0100

    Program.cs cleanup

--- src/Auth/Program.cs
+++ src/Auth/Program.cs
@@ -1,25 +1,16 @@
-﻿using System.Threading.Tasks;
-using Classic.Auth.Extensions;
+﻿using Classic.Auth.Extensions;
 using Classic.Shared;
 using Classic.Shared.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Classic.Auth;
-
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        await Host.CreateDefaultBuilder()
-            .ConfigureLogging(logging => logging
-                .AddConsole()
-                .SetMinimumLevel(LogLevel.Trace))
-            .ConfigureServices(services => services
-                .AddSingleton<ErrorHandler>()
-                .AddSharedServices()
-                .AddAuthenticationServer())
-            .RunConsoleAsync();
-    }
-}
+await Host.CreateDefaultBuilder()
+    .ConfigureLogging(logging => logging
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Trace))
+    .ConfigureServices(services => services
+        .AddSingleton<ErrorHandler>()
+        .AddSharedServices()
+        .AddAuthenticationServer())
+    .RunConsoleAsync();
--- src/World/Program.cs
+++ src/World/Program.cs
@@ -1,25 +1,16 @@
-﻿using System.Threading.Tasks;
-using Classic.Shared;
+﻿using Classic.Shared;
 using Classic.Shared.Extensions;
 using Classic.World.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Classic.World;
-
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        await Host.CreateDefaultBuilder()
-            .ConfigureLogging(logging => logging
-                .AddConsole()
-                .SetMinimumLevel(LogLevel.Trace))
-            .ConfigureServices(services => services
-                .AddSingleton<ErrorHandler>()
-                .AddSharedServices()
-                .AddWorldServer())
-            .RunConsoleAsync();
-    }
-}
+await Host.CreateDefaultBuilder()
+    .ConfigureLogging(logging => logging
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Trace))
+    .ConfigureServices(services => services
+        .AddSingleton<ErrorHandler>()
+        .AddSharedServices()
+        .AddWorldServer())
+    .RunConsoleAsync();
