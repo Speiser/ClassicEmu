fd8a72b3bdb8e916edb36d4d4fea34427816ab70
Bernhard Speiser <bernhard.speiser@yahoo.com>
2023-11-04 15:30:25 +0100

    appsettings

---
+++ src/Shared/Data/SQL/AuthDatabaseBaseScript.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS accounts
+(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(32) UNIQUE NOT NULL,
+    session_key BYTEA
+    -- TODO: verifier
+);
+
+CREATE INDEX IF NOT EXISTS accounts_username_idx ON accounts (username);
---
+++ src/Shared/Services/AuthDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Classic.Shared.Services;
+
+public class AuthDatabase
+{
+    public AuthDatabase(string connectionString)
+    {
+
+    }
+
+    public async Task Initialize()
+    {
+        var initializeScript = await File.ReadAllTextAsync("Data/SQL/AuthDatabaseBaseScript.sql");
+    }
+}
